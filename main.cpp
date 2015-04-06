52c45907944acaa82576318774e65538d545c493
Maximiliano Ejberowicz <maxi.ejberowicz@graion.com>
2015-04-05 22:18:29 -0300

    reading file from the main program

--- main.cpp
+++ main.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <conio.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    ifstream readFile("data/unlabeledTrainData.tsv");
+    string id, review, line;
+    bool firstLine = true;
+
+    if(!readFile.is_open() ){
+        cout << "Cannot read the file." << endl ;
+    }
+    else
+    {
+        while(getline(readFile,line))
+        {
+            if(firstLine)
+            {
+                firstLine = false;
+                continue;
+            }
+            stringstream iss(line);
+            getline(iss, id, '\t');
+            getline(iss, review, '\n');
+            cout << id << endl ;
+            cout << review << endl ;
+            getch();
+        }
+        readFile.close();
+    }
     return 0;
 }
