c75b00ac6de93225ffe078e21a795e100f686997
Maximiliano Ejberowicz <maxi.ejberowicz@graion.com>
2015-04-05 23:11:19 -0300

    labeledTrainData file + sentiment field

--- main.cpp
+++ main.cpp
@@ -2,14 +2,13 @@
 #include <string>
 #include <fstream>
 #include <sstream>
-#include <conio.h>
 
 using namespace std;
 
 int main()
 {
-    ifstream readFile("data/unlabeledTrainData.tsv");
-    string id, review, line;
+    ifstream readFile("data/labeledTrainData.tsv");
+    string id, review, sentiment, line;
     bool firstLine = true;
 
     if(!readFile.is_open() ){
@@ -26,10 +25,12 @@ int main()
             }
             stringstream iss(line);
             getline(iss, id, '\t');
+            getline(iss, sentiment, '\t');
             getline(iss, review, '\n');
             cout << id << endl ;
+            cout << sentiment << endl ;
             cout << review << endl ;
-            getch();
+            getchar();
         }
         readFile.close();
     }
