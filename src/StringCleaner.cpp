990169a1c879825fe0c0cf7eceb61049736361e5
Maximiliano Ejberowicz <maxi.ejberowicz@graion.com>
2015-04-11 16:01:34 -0300

    Regex commented out for now

--- src/StringCleaner.cpp
+++ src/StringCleaner.cpp
@@ -21,9 +21,9 @@ void StringCleaner::CleanNonLetters(std::string* toClean)
 {
     try
     {
-        std::regex lettersRegex("[^A-Za-z\s]");
+        //std::regex lettersRegex("[^A-Za-z\s]");
         std::cout << *(toClean) << std::endl;
-        *toClean = std::regex_replace(*toClean, lettersRegex, std::string(""));
+        //*toClean = std::regex_replace(*toClean, lettersRegex, std::string(""));
     }
     catch (std::regex_error& e) {
         // Syntax error in the regular expression
