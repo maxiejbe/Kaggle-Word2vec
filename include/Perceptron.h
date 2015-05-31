0d141c7b74ba91934ca810a9bf3f44c8c97d28cb
Santiago Alcala <santyalcala.dbr@hotmail.com>
2015-05-31 03:17:54 -0300

    Empece Perceptron

--- include/Perceptron.h
+++ include/Perceptron.h
@@ -1,14 +1,49 @@
 #ifndef PERCEPTRON_H
 #define PERCEPTRON_H
+#include "PerceptronEntry.h"
+
+#include <vector>
+#include<string>
+
+using namespace std;
 
 
-class Perceptron
-{
+
+class PerceptronEntry{
+
+    public: vector<int> vectorReview;
+    public: int label;
+
+    public:
+        void setVectorReview(vector<int> vectorReviewIn);
+        void setLabel(int labelIn);
+        vector<int> getVectorReview();
+        int getLabel();
+};
+
+class PerceptronOutput{
+    public: string movieID;
+    public: double probability;
+
+    public:
+        void   setMovieID(string movieIDIn);
+        void   setProbability(double probabilityIn);
+        string getMovieID();
+        double getProbability();
+
+};
+
+class Perceptron{
     public:
         Perceptron();
         virtual ~Perceptron();
+
+    static vector<double>* trainPerceptron(vector<PerceptronEntry> vectorIn, int dimension);
+    static vector<PerceptronOutput> testPerceptron(vector<double> weights, vector<PerceptronEntry> entryToPredict);
+
     protected:
     private:
+
 };
 
 #endif // PERCEPTRON_H
--- src/Perceptron.cpp
+++ src/Perceptron.cpp
@@ -1,5 +1,7 @@
 #include "Perceptron.h"
 
+using namespace std;
+
 Perceptron::Perceptron()
 {
     //ctor
@@ -9,3 +11,89 @@ Perceptron::~Perceptron()
 {
     //dtor
 }
+
+void PerceptronEntry::setVectorReview(vector<int> vectorReviewIn){
+    this->vectorReview = vectorReviewIn;
+}
+
+void PerceptronEntry::setLabel(int labelIn){
+    this->label = labelIn;
+}
+vector<int> PerceptronEntry::getVectorReview(){
+
+    return this->vectorReview;
+}
+int PerceptronEntry::getLabel(){
+    return this->label;
+}
+
+
+void PerceptronOutput::setMovieID(string movieIDIn){
+    this->movieID = movieIDIn;
+}
+void PerceptronOutput::setProbability(double probabilityIn){
+    this->probability = probabilityIn;
+}
+string PerceptronOutput::getMovieID(){
+    return this->movieID;
+}
+double PerceptronOutput::getProbability(){
+    return this->probability;
+}
+
+double dotProduct(vector<double>* vec1, vector<int> vec2){
+
+    int dimension = vec2.size();
+    double total = 0 ;
+
+    int a, c ;
+    int* b;
+
+    c = a*(*b);
+
+
+    for (int i=0; i<=dimension-1; i++){
+
+        }
+
+}
+
+
+vector<double>* Perceptron::trainPerceptron(vector<PerceptronEntry> vectorIn, int dimension){
+
+    int vectorDimension = vectorIn.size();
+    vector<int> vectorIntAux;
+    PerceptronEntry perceptronEntryAux;
+    int labelAux;
+    int resultado=0;
+    double result;
+
+
+    vector<double>* weights = new vector<double>(dimension);
+
+    for (int i=0; i <= vectorDimension ; i++){
+        perceptronEntryAux = vectorIn[i];
+
+        vectorIntAux = perceptronEntryAux.getVectorReview();
+        labelAux = perceptronEntryAux.getLabel();
+
+        if (dotProduct(weights, vectorIntAux)>0,5){
+            resultado = 1;
+        }
+        else{
+            resultado = 0;
+        };
+
+
+
+
+
+
+
+
+    return weights;
+    }
+
+}
+
+
